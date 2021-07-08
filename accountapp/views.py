66be7d9d82d667685ea7bb15655df1a678788893
noeul1114 <parkhyongsok@naver.com>
2021-07-08 09:28:55 +0900

    7/8 5ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'GET METHOD!'})
+
+
