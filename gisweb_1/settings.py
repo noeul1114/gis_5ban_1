e146b930a92b783ed9933e33de4953841dfb3d8c
noeul1114 <parkhyongsok@naver.com>
2021-07-13 16:13:06 +0900

    7/13 5ban class commit 4

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 local_env = open(os.path.join(BASE_DIR, '.env'))
@@ -143,3 +145,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
