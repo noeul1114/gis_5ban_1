31fef70915fdec24c1f188f6562509f54ed4880d
noeul1114 <parkhyongsok@naver.com>
2021-07-15 10:12:21 +0900

    7/15 5ban class commit 2

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
