23c223e665214fd3cbb6d7e0e46f974bb6139262
noeul1114 <parkhyongsok@naver.com>
2021-07-22 10:39:29 +0900

    7/22 5ban class commit 3

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -138,9 +138,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
