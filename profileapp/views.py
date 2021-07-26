bae355044cee7a3b02cda6b0f7dc0f239d06c279
noeul1114 <parkhyongsok@naver.com>
2021-07-26 10:31:07 +0900

    7/26 class 5ban commit 4

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+
+
+
+
+
+
