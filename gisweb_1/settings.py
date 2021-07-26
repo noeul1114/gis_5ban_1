1b5a6cd4c1893c05379cd2f485d6e3fc6e37af33
noeul1114 <parkhyongsok@naver.com>
2021-07-26 09:16:57 +0900

    7/26 class 5ban commit 1

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
--- gisweb_1/urls.py
+++ gisweb_1/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls'))
+    path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
 ]
+
+
