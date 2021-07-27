11b3b149206ef0c3f3ff5fb501788e6f42b0cf5f
noeul1114 <parkhyongsok@naver.com>
2021-07-27 14:53:46 +0900

    7/27 class 5ban commit 1

--- profileapp/views.py
+++ profileapp/views.py
@@ -14,6 +14,10 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 
 
 
