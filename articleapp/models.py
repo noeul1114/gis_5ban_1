f6a650519a23296a5a438624875331899f2a40db
noeul1114 <parkhyongsok@naver.com>
2021-08-02 10:37:59 +0900

    8/2 class 5ban commit 4

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    writer = models.ForeignKey(User,
+                               on_delete=models.SET_NULL,
+                               related_name='article',
+                               null=True)
+
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=True)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
