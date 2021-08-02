4ea062c29e70dae6674e54ed8b93d9eda78ac0a4
noeul1114 <parkhyongsok@naver.com>
2021-08-02 09:15:44 +0900

    8/2 class 5ban commit 1

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,4 +1,8 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'articleapp'
 
 urlpatterns = [
-
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
 ]
