ad6988c0d406189bf48bcc0ac4e72a7420e96609
noeul1114 <parkhyongsok@naver.com>
2021-08-03 14:48:30 +0900

    8/3 class 5ban commit 1

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from articleapp.views import ArticleCreateView
+
 app_name = 'articleapp'
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+
+    path('create/', ArticleCreateView.as_view(), name='create'),
 ]
+
+
+
+
+
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
+
+
+
