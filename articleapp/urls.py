3255e0c1779a8eb2285835da5be7772e3ee1434f
noeul1114 <parkhyongsok@naver.com>
2021-08-03 15:51:13 +0900

    8/3 class 5ban commit 3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
 
 app_name = 'articleapp'
 
@@ -10,6 +10,7 @@ urlpatterns = [
 
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
 ]
 
 
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -24,5 +24,14 @@ class ArticleDetailView(DetailView):
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
 
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
