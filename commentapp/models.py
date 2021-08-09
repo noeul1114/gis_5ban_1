706f4bbf3ca090523a9a0ff778c321e8291b151f
noeul1114 <parkhyongsok@naver.com>
2021-08-09 10:07:54 +0900

    8/9 class 5ban commit 3

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                related_name='comment', null=True)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True)
+
+    content = models.TextField(null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
