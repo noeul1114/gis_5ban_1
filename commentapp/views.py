54650f363b8c6698d9beacfdb12ae1abbb9f99b4
noeul1114 <parkhyongsok@naver.com>
2021-08-09 10:29:02 +0900

    8/9 class 5ban commit 4

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from commentapp.forms import CommentCreationForm
+from commentapp.models import Comment
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/create.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
