a9a9c26f09ad257d5267f77c086301d1bfdcc72b
noeul1114 <parkhyongsok@naver.com>
2021-08-12 10:40:48 +0900

    8/12 class 5ban commit 5

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'projectapp',
 ]
 
 MIDDLEWARE = [
--- gisweb_1/urls.py
+++ gisweb_1/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
