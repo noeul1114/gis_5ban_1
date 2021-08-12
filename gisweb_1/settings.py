8746ae709fc3fb47afd5bacc906821105aafe622
noeul1114 <parkhyongsok@naver.com>
2021-08-12 09:22:33 +0900

    8/12 class 5ban commit 1

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = env_list['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
