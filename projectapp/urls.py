b3f33979c77816830ba35bdf418a4962042b5bc0
noeul1114 <parkhyongsok@naver.com>
2021-08-17 16:05:00 +0900

    8/17 class 5ban commit 5

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 app_name = 'projectapp'
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+
+    path('list/', ProjectListView.as_view(), name='list'),
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -25,5 +25,12 @@ class ProjectDetailView(DetailView):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
+    paginate_by = 20
 
 
