e5ef0505ab0e5487831f437bb980800b0fd0b4d1
noeul1114 <parkhyongsok@naver.com>
2021-08-17 14:42:35 +0900

    8/17 class 5ban commit 1

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='project/', null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
