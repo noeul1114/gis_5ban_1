65193588fb420024b1e408c97cecdb27a4a2e19e
noeul1114 <parkhyongsok@naver.com>
2021-08-17 15:46:03 +0900

    8/17 class 5ban commit 4

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,18 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
-    success_url = reverse_lazy('articleapp:list')
     template_name = 'projectapp/create.html'
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
 class ProjectDetailView(DetailView):
