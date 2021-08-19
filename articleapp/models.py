71d218e94d1a623efdf12dfca0df2987a98ee479
noeul1114 <parkhyongsok@naver.com>
2021-08-19 09:26:36 +0900

    8/19 class 5ban commit 2

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
 
 
 class Article(models.Model):
@@ -9,6 +10,10 @@ class Article(models.Model):
                                on_delete=models.SET_NULL,
                                related_name='article',
                                null=True)
+    project = models.ForeignKey(Project,
+                                on_delete=models.SET_NULL,
+                                related_name='article',
+                                null=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
--- projectapp/models.py
+++ projectapp/models.py
@@ -9,3 +9,6 @@ class Project(models.Model):
     image = models.ImageField(upload_to='project/', null=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
