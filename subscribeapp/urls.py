71ee81733c580aa8de816c08654857948a4ea46a
noeul1114 <parkhyongsok@naver.com>
2021-08-23 09:43:21 +0900

    8/23 class 5ban commit 2

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+
+from subscribeapp.views import SubscriptionView
+
+app_name = 'subscribeapp'
 
 urlpatterns = [
-
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
 ]
--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from projectapp.models import Project
+from subscribeapp.models import Subscription
+
+
+@method_decorator(login_required, 'get')
+class SubscriptionView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        project = Project.objects.get(pk=kwargs['project_pk'])
+
+        subscription = Subscription.objects.filter(user=user,
+                                                   project=project)
+
+        if subscription.exists():
+            subscription.delete()
+        else:
+            Subscription(user=user, project=project).save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
