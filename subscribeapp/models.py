e93dfe38e22ece74d019473077fb7740362338da
noeul1114 <parkhyongsok@naver.com>
2021-08-23 09:16:07 +0900

    8/23 class 5ban commit 1

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription', null=False)
+
+    class Meta:
+        unique_together = ['user', 'project']
+
