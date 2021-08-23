7fa846656eb0b2abdec695fb81170699c66a3f48
noeul1114 <parkhyongsok@naver.com>
2021-08-23 10:47:56 +0900

    8/23 class 5ban commit 4

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -29,3 +30,20 @@ class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
+
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+
+    paginate_by = 20
+
+    def get_queryset(self):
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list
+
+
+
+
