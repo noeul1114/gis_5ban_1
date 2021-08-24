baabf3e1f35a2ddcf67a19edf2452012ff3e1bc1
noeul1114 <parkhyongsok@naver.com>
2021-08-24 15:52:12 +0900

    8/24 class 5ban commit 4

--- likeapp/urls.py
+++ likeapp/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
 
+from likeapp.views import LikeArticleView
+
+app_name = 'likeapp'
 
 urlpatterns = [
-
+    path('article/<int:article_pk>', LikeArticleView.as_view(), name='article_like'),
 ]
--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from articleapp.models import Article
+from likeapp.models import LikeRecord
+
+
+@method_decorator(login_required, 'get')
+class LikeArticleView(RedirectView):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        article = Article.objects.get(pk=kwargs['article_pk'])
+
+        like_record = LikeRecord.objects.filter(user=user,
+                                                article=article)
+        if like_record.exists():
+            return HttpResponseRedirect(reverse('articleapp:detail',
+                                                kwargs={'pk': kwargs['article_pk']}))
+        else:
+            LikeRecord(user=user, article=article).save()
+        article.like += 1
+        article.save()
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']})
+
