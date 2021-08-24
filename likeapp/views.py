bb90ef0f38c669dda01d091ee82294b680f2d651
noeul1114 <parkhyongsok@naver.com>
2021-08-24 16:16:23 +0900

    8/24 class 5ban commit 5

--- likeapp/views.py
+++ likeapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView
 
@@ -11,12 +11,11 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
-@method_decorator(login_required, 'get')
+@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'get')
 class LikeArticleView(RedirectView):
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
-
         like_record = LikeRecord.objects.filter(user=user,
                                                 article=article)
         if like_record.exists():
