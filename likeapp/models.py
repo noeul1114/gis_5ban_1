70d5f1d1519e19a8f1f357de5edc379de28f4e19
noeul1114 <parkhyongsok@naver.com>
2021-08-24 15:09:22 +0900

    8/24 class 5ban commit 3

--- likeapp/models.py
+++ likeapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class LikeRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='like_record', null=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='like_record', null=False)
+
+    class Meta:
+        unique_together = ['user', 'article']
