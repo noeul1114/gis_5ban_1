1ff6995ffe660d5d359dc48302474b35d73d2f92
noeul1114 <parkhyongsok@naver.com>
2021-08-26 09:41:10 +0900

    8/26 class 5ban commit 2

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -63,6 +63,12 @@ INSTALLED_APPS = [
     'likeapp',
 ]
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
