67fcb4dee0f0025b747e87ea6ebf756a8eb264c4
noeul1114 <parkhyongsok@naver.com>
2021-08-26 10:23:48 +0900

    8/26 class 5ban commit 3

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -12,24 +14,35 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    article.like += 1
+    article.save()
+
+    like_record = LikeRecord.objects.filter(user=user,
+                                            article=article)
+    if like_record.exists():
+        raise ValidationError('like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+
 @method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'get')
 class LikeArticleView(RedirectView):
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
-        like_record = LikeRecord.objects.filter(user=user,
-                                                article=article)
-        if like_record.exists():
-            # 좋아요가 반영 X
+
+        try:
+            db_transaction(user, article)
+            # 좋아요 반영 O
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except ValidationError:
+            # 좋아요 반영 X
             messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail',
                                                 kwargs={'pk': kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-        article.like += 1
-        article.save()
-        # 좋아요가 반영 O
-        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
