9044174e67be3f06ba19f8261b8776aa2cbaaa92
noeul1114 <parkhyongsok@naver.com>
2021-08-26 09:25:36 +0900

    8/26 class 5ban commit 1

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -19,12 +20,16 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user,
                                                 article=article)
         if like_record.exists():
+            # 좋아요가 반영 X
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail',
                                                 kwargs={'pk': kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
         article.like += 1
         article.save()
+        # 좋아요가 반영 O
+        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
