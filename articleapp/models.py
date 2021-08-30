9d067111ea04ac075164dfb1455317adf8632787
noeul1114 <parkhyongsok@naver.com>
2021-08-30 10:12:02 +0900

    8/30 class 5ban commit 4

--- articleapp/models.py
+++ articleapp/models.py
@@ -13,7 +13,7 @@ class Article(models.Model):
     project = models.ForeignKey(Project,
                                 on_delete=models.SET_NULL,
                                 related_name='article',
-                                null=True)
+                                null=True, blank=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
